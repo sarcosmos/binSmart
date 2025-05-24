75462939dbec3b0d6ad6b9a02035b3eefc9faf38
Chamir <samir.castillo.r@gmail.com>
2025-05-24 13:16:27 -0500

    ajuste a vite 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,19 @@
-import vue from '@vitejs/plugin-vue';
-import laravel from 'laravel-vite-plugin';
-import path from 'path';
-import tailwindcss from "@tailwindcss/vite";
-import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
-        tailwindcss(),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        vue(),
     ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './resources/js'),
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
-        },
-    },
     build: {
         manifest: true,
         outDir: 'public/build',
-        rollupOptions: {
-            input: 'resources/js/app.ts',
-        },
+        emptyOutDir: true,
     },
-    base: '/build/',
 });
 
